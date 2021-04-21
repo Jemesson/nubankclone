0cb48890281b19835b68565d2dec9b3e065f75db
Jemesson <jemessonlima@gmail.com>
2021-04-21 19:53:50 -0300

    Add nubank layout

---
+++ NubankClone/ButtonDesignable.swift
@@ -0,0 +1,39 @@
+//
+//  ButtonDesignable.swift
+//  NubankClone
+//
+//  Created by Jemesson Lima on 21/04/21.
+//
+
+import UIKit
+
+@IBDesignable extension UIButton {
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+    }
+}
--- NubankClone/ViewController.swift
+++ NubankClone/ViewController.swift
@@ -2,18 +2,13 @@
 //  ViewController.swift
 //  NubankClone
 //
-//  Created by Ezilda Lima on 21/04/21.
+//  Created by Jemesson Lima on 21/04/21.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
